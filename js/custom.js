1366efbaf45cc9cb92d992d42586e045e1383172
Kapil Lohakare <kapil.lohakare@clariontechnologies.co.in>
2016-04-14 17:41:17 +0530

    Added Page titles to widgets

--- js/custom.js
+++ js/custom.js
@@ -8,18 +8,41 @@ function checkHistory(targetClass, count) {
         var insert = true;
         var sp = history.toString().split(",");
         htmlContent += '<ul>';
-         var getLocation = function(href) {
-    var l = document.createElement("a");
-    l.href = href;
-    return l;
-};
+        var getLocation = function (href) {
+            var l = document.createElement("a");
+            l.href = href;
+            return l;
+        };
+        var getPageName = function (slug) {
+            var demo = '';
+            $.ajaxSetup({async: false});
+            $.post(
+                    script_object.ajaxurl,
+                    {
+                        'action': 'last_visited_getpage',
+                        'data': {
+                            'slug': slug
+                        },
+                    }
+            ).done(function (data) {
+                if (data.length) {
+                    demo = data;
+                }
+            });
+            $.ajaxSetup({async: true}); //So as to avoid any other ajax calls made sybchrounously
+            return demo;
+        };
         for (var i = sp.length - 1; i >= 0; i--) {
-          var page=(getLocation(sp[i]).pathname).split("/");
-            var page_slug=page[page.length-2];
+            var page = (getLocation(sp[i]).pathname).split("/");
+            var page_slug = page[page.length - 2];
+            var page_title = getPageName(page_slug);
+            if (page_title == "") {
+                page_title = sp[i];
+            }
             htmlContent += '<li><a class="demo-pricing demo-pricing-1"  href="'
                     + sp[i]
                     + '">'
-                    + page_slug + '</a></li>';
+                    + page_title + '</a></li>';
             if (sp[i] == document.URL) {
                 insert = false;
             }
--- last-visited-pages.php
+++ last-visited-pages.php
@@ -14,12 +14,39 @@ include(plugin_dir_path(__FILE__) . '/widget-last-visited.php' );
 function last_visited_enqueue_scripts() {
     wp_enqueue_script("jquery");
     wp_enqueue_script('jquery.cookie', plugins_url('js/jquery.cookie.js', __FILE__));
-    wp_enqueue_script('cookie-custom', plugins_url('js/custom.js', __FILE__));
+    // Register the script
+wp_register_script( 'script_handle', plugins_url('js/custom.js', __FILE__) );
+// Localize the script with new data
+$translation_array = array(
+	'ajaxurl' => admin_url('admin-ajax.php')
+);
+wp_localize_script( 'script_handle', 'script_object', $translation_array );
+wp_enqueue_script( 'script_handle' );
+    
+    
+    
+    
+
 }
 
 //This hook ensures our scripts and styles are only loaded in the admin.
 add_action('wp_enqueue_scripts', 'last_visited_enqueue_scripts');
 
+add_action( 'wp_ajax_last_visited_getpage', 'last_visited_getpage' );
+add_action( 'wp_ajax_nopriv_last_visited_getpage', 'last_visited_getpage' );
+
+function last_visited_getpage() {
+	$req = $_POST['data'];
+//        echo '<pre>';print_r($req);echo '</pre>';
+//	$arr = explode("&",$req);
+//	$vote = $arr[0];
+//	$userid = $arr[1].":".$vote;
+//	$productid = $arr[2];
+//	$count = 0;
+        $page = get_page_by_path($req['slug']);
+$title = get_the_title($page->ID);
+	echo $title; die();
+}
 /**
  * Flushes all Options
  *
